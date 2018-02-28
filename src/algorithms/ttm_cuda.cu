7a7586020e3663436ff2861ff186635ab44f764b
Star Brilliant <coder@poorlab.com>
2018-03-01 03:24:08 +0800

    ttm_cuda debug

--- src/algorithms/ttm_cuda.cu
+++ src/algorithms/ttm_cuda.cu
@@ -137,6 +137,7 @@ SparseTensor tensor_times_matrix_cuda(SparseTensor& X, Tensor& U, size_t mode, C
 
     Timer timer_kernel(cuda_dev->device_id);
     timer_kernel.start();
+    std::fprintf(stderr, "[CUDA TTM Kernel] Launch ttm_cuda_kernel<<<%zu, (%zu, %zu), 0>>()\n", Y.num_chunks, Y_num_subchunks, Y_subchunk_size);
     ttm_cuda_kernel<<<Y.num_chunks, dim3(Y_num_subchunks, Y_subchunk_size), 0>>>(dev_fiberidx, X_indices_m, nrows, ncols, Y.chunk_size, Y_subchunk_size, X.chunk_size, Ustride, Y_values, X_values, U_values);
     int result = cudaThreadSynchronize();
     timer_kernel.stop();
